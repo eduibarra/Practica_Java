ec308d82329b56852f23b5b392efdcd249de9ec5
Juan Eduardo <ibarraedu@gmail.com>
2018-12-08 18:04:48 -0300

    se completo el ejercicio

---
+++ EjemploHerencia/src/ejemploherencia/Cliente.java
@@ -0,0 +1,40 @@
+package ejemploherencia;
+import java.util.Date;
+
+public class Cliente extends Persona{
+    private int idCliente;
+    private java.util.Date fechaRegistro;
+    private boolean vip;
+    private static int contadorClientes;
+    
+    public Cliente(Date fechaRegistro, boolean vip){
+        this.idCliente = ++contadorClientes;
+        this.fechaRegistro = fechaRegistro;
+        this.vip = vip;
+    }
+    
+    public int getIdcliente(){
+        return idCliente;
+    }
+    
+    public Date getFechaRegistro(){
+        return fechaRegistro;
+    }
+    
+    public boolean isVip(){
+        return vip;
+    }
+    
+    public void setVip(boolean vip){
+        this.vip = vip;
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString() + " Cliente {\n" 
+                                + " IdCliente: " + idCliente
+                                + " Fecha de Registro: " + fechaRegistro
+                                + " VIP: " + vip +" \n}"; 
+    }
+    
+}
--- EjemploHerencia/src/ejemploherencia/EjemploHerencia.java
+++ EjemploHerencia/src/ejemploherencia/EjemploHerencia.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejemploherencia;
-
-/**
- *
- * @author edu_p
- */
 public class EjemploHerencia {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Empleado e1 = new Empleado("Juancito", 10000);
+        System.out.println("Mostramos el empleado e1: ");
+        System.out.println(e1);
+        
+        Empleado e2 = new Empleado("Pedro", 12000);
+        e2.setEdad(25);
+        e2.setGenero('M');
+        e2.setDireccion("Buenos Aires, Argentina");
+        System.out.println("\nMostramos el empleado e2: ");
+        System.out.println(e2);
+        
+        Cliente c1 = new Cliente(new java.util.Date(), false);
+        System.out.println("Mostramos el Cliente c1: ");
+        System.out.println(c1);
+        // Mostramos mas datos...
+        c1.setNombre("Martita");
+        c1.setEdad(32);
+        c1.setGenero('F');
+        c1.setVip(true);
+        c1.setDireccion("Santiago del Estero, Argentina");
+        System.out.println("\nVolvemos a mostrar el cliente c1 con mas datos:");
+        System.out.println(c1);
     }
     
 }
